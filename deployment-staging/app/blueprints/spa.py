83c68ecd4745def3960d1096d5fe01b194e1d332
achasol-db <naim.achahboun@databricks.com>
2025-10-14 15:17:14 +0200

    fixes

---
+++ deployment-staging/app/blueprints/spa.py
@@ -0,0 +1,27 @@
+import os
+from flask import Blueprint, send_from_directory, current_app
+
+spa_bp = Blueprint("spa", __name__)
+
+
+@spa_bp.route('/', defaults={'path': ''})
+@spa_bp.route('/<path:path>')
+def serve_spa(path):
+    file_path = os.path.join(current_app.static_folder, path)
+    if path != "" and os.path.exists(file_path):
+        return send_from_directory(current_app.static_folder, path)
+    else:
+        return send_from_directory(current_app.static_folder, 'index.html')
+
+@spa_bp.route('/visualiser/', defaults={'path': ''})
+@spa_bp.route('/visualiser/<path:path>')
+def serve_visualiser_spa(path):
+    file_path = os.path.join(current_app.static_folder + "/visualiser", path)
+    if path != "" and os.path.exists(file_path):
+        return send_from_directory(current_app.static_folder + "/visualiser", path)
+    else:
+        return send_from_directory(current_app.static_folder + "/visualiser", 'index.html')
+
+@spa_bp.app_errorhandler(404)
+def handle_404(e):
+    return send_from_directory(current_app.static_folder, 'index.html')
